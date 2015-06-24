01dd02a5dd2409eada2a3fdfc260e47f87b85b3e
Isaiah Lee <rushfive@uw.edu>
2015-06-24 10:55:56 -0700

    readme update

--- UserService/AllUsers.cs
+++ UserService/AllUsers.cs
@@ -15,51 +15,17 @@ namespace UserService
             Users = Users.Where(u => u.ConnectionId != connectionId).ToList();
         }
 
-        //public static bool UserNotInList(string connectionId)
-        //{
-        //    for (int i = 0; i < Users.Count; i++)
-        //    {
-        //        if (Users[i].ConnectionId == connectionId)
-        //            return false;
-        //    }
-
-        //    return true;
-        //}
-
         public static bool UserNotInList(string connectionId)
         {
             var lookingFor = Users.Where(u => u.ConnectionId == connectionId).FirstOrDefault();
             return (lookingFor == null) ? true : false;
         }
 
-        //public static User GetUserBy(string connectionId)
-        //{
-        //    User user = new User();
-
-        //    for (int i = 0; i < Users.Count; i++)
-        //    {
-        //        if (Users[i].ConnectionId == connectionId)
-        //        {
-        //            user = Users[i];
-        //        }
-        //    }
-
-        //    return user;
-        //}
-
         public static User GetUserBy(string connectionId)
         {
             var user = Users.Where(u => u.ConnectionId == connectionId).FirstOrDefault();
             return user;
         }
-
-        //public static User GetUserByUsername(string username)
-        //{
-        //    var user = Users.Where(u => u.UserName == username).FirstOrDefault();
-        //    return user;
-        //}
-
-
 
     }
 }
